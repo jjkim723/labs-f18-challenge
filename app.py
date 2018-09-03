3d1600f718481f3271db31f1e81222a43b78b817
Jonathan Kim <Insomnia@dyn-160-39-138-214.dyn.columbia.edu>
2018-09-03 15:58:35 -0400

    finished

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def main():
-    return render_template('index.html')
+@app.route('/pokemon/<query>', methods=['GET'])
+def main(query=1):
+	# GET
+	res = requests.get('http://pokeapi.co/api/v2/pokemon/' + query)
+	info = res.json()
+	print(info)
+	# Display
+	if query.isdigit():
+		line = 'The pokemon with id ' + str(info['id']) + ' is ' + info['name']
+		return render_template('index.html', line=line)
+	line = info['name'] + ' has' + ' id ' + str(info['id'])
+	return render_template('index.html', line=line)
 
 
 if __name__ == '__main__':
